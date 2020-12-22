22c0990136d36de14c8bb5fd70cec65c84cde714
edenchengsc <edencheng2020@gmail.com>
2020-12-21 17:22:10 -0800

    Add util files

---
+++ utilFiles/commandFiles/start mq.bat
@@ -0,0 +1,5 @@
+
+start cmd /k mqnamesrv
+
+start cmd /k mqbroker -n 127.0.0.1:9876 autoCreateTopicEnable=true
+
---
+++ utilFiles/commandFiles/start redis.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\edenchengshu\App\Redis-x64-5.0.10"
+start cmd /k redis-server.exe redis.windows.conf 
