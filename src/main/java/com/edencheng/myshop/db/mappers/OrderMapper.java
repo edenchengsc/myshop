3eee6e75188c6a3786bc8247fa2a15ed82e40376
edenchengsc <edencheng2020@gmail.com>
2020-12-22 00:16:44 -0800

    mybatis auto generate mapper files

---
+++ src/main/java/com/edencheng/myshop/db/mappers/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.edencheng.myshop.db.mappers;
+
+import com.edencheng.myshop.db.po.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
--- utilFiles/database/create user table.sql
+++ utilFiles/database/create user table.sql
@@ -1,18 +1,8 @@
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+DROP TABLE IF EXISTS `myshop_user`;
+CREATE TABLE `myshop_user`  (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'user ID',
   `user_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'username',
   `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'address',
-  `phone` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'mobile number',
+  `phone` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'phone',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-
-SET FOREIGN_KEY_CHECKS = 1;
